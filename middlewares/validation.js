186d44842f3a6ba907125dc7fa322606d77f8d5c
AndreyZhuykov <intsaudio@gmail.com>
2022-06-01 22:54:26 +0300

    lvl-5

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -42,7 +42,7 @@ const movieValidation = celebrate({
 
 const movieIdValidation = celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().hex().length(16),
+    _id: Joi.string().hex().length(24),
   }),
 });
 
